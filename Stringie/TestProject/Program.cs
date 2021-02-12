ba12f49e047c8f3bd5064507dc94572293a89614
canro91 <canro91@hotmail.com>
2021-02-12 17:23:51 -0500

    Add dummy test project

---
+++ Stringie/TestProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using Stringie;
+using System;
+using System.Diagnostics;
+
+namespace TestProject
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var helloWorld = ", world!".Insert("Hello");
+            if (helloWorld != "Hola, mundo!")
+            {
+                throw new Exception("Your mission, Jim, should you decide to accept it, is to delete this project and write real unit tests. This code will self-destruct in five seconds. Good luck, Jim. 5...4...3...2...1...smoke coming out of your computer");
+            }
+
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+            }
+        }
+    }
+}
